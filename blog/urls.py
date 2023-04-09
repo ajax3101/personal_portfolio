2913f2e86e084132cdf0571acd59b2a1808acd64
Denys Morozov <ajax3101@gmail.com>
2023-04-09 17:34:49 +0300

    migrate

--- blog/urls.py
+++ blog/urls.py
@@ -19,5 +19,5 @@ from blog import views
 app_name = "blog"
 urlpatterns = [
     path('', views.all_blogs, name='all_blogs'),
-    path('<int:blog_id>', views.detail, name='detail'),
+    path('<int:blog_id>/', views.detail, name='detail'),
 ]
